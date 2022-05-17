de5c05e366b10e71366674b15ed5c719dc22a671
Tanmay Mishra <tanmaym830@gmail.com>
2022-05-17 15:29:16 +0530

    Updates

---
+++ precache-manifest.7fb3971764e4bdaeeb9d98183750caff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9210a646c5b1b83fc20f",
+    "url": "/TextUtils-React/static/css/main.116f952a.chunk.css"
+  },
+  {
+    "revision": "9210a646c5b1b83fc20f",
+    "url": "/TextUtils-React/static/js/main.9210a646.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils-React/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "a7552a635d7a5f76cda515931671084c",
+    "url": "/TextUtils-React/index.html"
+  }
+];
